67ec6055830f66ad9b3e4f083b591cf65346b2a1
ryanlyn <mailboxryanlin@icloud.com>
2020-05-10 15:38:08 +1000

    Prepare for 0.0.1.post1

--- logcraft/__init__.py
+++ logcraft/__init__.py
@@ -1,5 +1,5 @@
-__MAJOR = 0
-__MINOR = 0
-__MICRO = 1
+__MAJOR = "0"
+__MINOR = "0"
+__MICRO = "1.post1"
 
-__VERSION__ = '{}.{}.{}'.format(__MAJOR, __MINOR, __MICRO)
+__VERSION__ = "{}.{}.{}".format(__MAJOR, __MINOR, __MICRO)
